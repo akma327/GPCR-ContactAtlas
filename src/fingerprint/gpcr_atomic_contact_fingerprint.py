fddf7e1bdfe52d4e9b3ae8313712fc950561eaa8
Anthony <akma327@stanford.edu>
2017-05-31 15:07:26 -0700

    Full implementation of gpcr atomic contact fingerprint

--- src/fingerprint/gpcr_atomic_contact_fingerprint.py
+++ src/fingerprint/gpcr_atomic_contact_fingerprint.py
@@ -63,8 +63,8 @@ def get_edge_and_frameDict(sorted_input_files, labels):
 	frameDict = {}
 	edge_dict = {}
 
-	# residue_to_atomic_contacts = {gpcrdb1:gpcrdb2: {receptor_index1: [list of atomic resolution contacts], ...}}
-	# {3x53:3x54: {0:[ALA168-N:VAL169-N, ALA168-O:VAL169-N,...], 1: []}}
+	# residue_to_atomic_contacts = {itype: {gpcrdb1:gpcrdb2: {receptor_index1: [list of atomic resolution contacts], ...}}}
+	# {"hbbb": {3x53:3x54: {0:[ALA168-N:VAL169-N, ALA168-O:VAL169-N,...], 1: []}}}
 	residue_to_atomic_contacts = {} 
 	for i in range(len(sorted_input_files)):
 		p, label = sorted_input_files[i], labels[i]
@@ -79,7 +79,18 @@ def get_edge_and_frameDict(sorted_input_files, labels):
 				edge_dict[itype][(gpcrdb1, gpcrdb2)] = []
 			edge_dict[itype][(gpcrdb1, gpcrdb2)].append(i)
 
-	return edge_dict, frameDict
+
+			### Atomic level contacts encoding into flareplot json
+			gpcrdb_pair = (gpcrdb1, gpcrdb2)
+			if(itype not in residue_to_atomic_contacts):
+				residue_to_atomic_contacts[itype] = {}
+			if(gpcrdb_pair not in residue_to_atomic_contacts[itype]):
+				residue_to_atomic_contacts[itype][gpcrdb_pair] = {}
+			if(atom_list not in residue_to_atomic_contacts[itype][gpcrdb_pair]):
+				residue_to_atomic_contacts[itype][gpcrdb_pair][i] = []
+			residue_to_atomic_contacts[itype][gpcrdb_pair][i].append(atom_list)
+
+	return edge_dict, frameDict, residue_to_atomic_contacts
 
 
 
@@ -88,7 +99,7 @@ def fingerprint(OUTPUT_DIR, INPUT_FILES):
 		os.makedirs(OUTPUT_DIR)
 
 	sorted_input_files, labels = sort_paths(INPUT_FILES)
-	edge_dict, frameDict = get_edge_and_frameDict(sorted_input_files, labels)
+	edge_dict, frameDict, residue_to_atomic_contacts = get_edge_and_frameDict(sorted_input_files, labels)
 	for itype in edge_dict:
 		out_path = OUTPUT_DIR + "/" + itype + ".json"
 		edges = []
@@ -100,6 +111,11 @@ def fingerprint(OUTPUT_DIR, INPUT_FILES):
 		json_dict = partial_input
 		json_dict["edges"] = edges
 		json_dict["frameDict"] = frameDict
+
+		### Atomic level contacts encoding into flareplot json
+		json_dict["residue_to_atomic_contacts"] = residue_to_atomic_contacts[itype]
+
+
 		with open(out_path, 'w') as outfile:
 			json.dump(json_dict, outfile)
 
